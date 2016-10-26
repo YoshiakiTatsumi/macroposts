fea49b3d771b1c9f36d464283fbafeaa3feed55f
YoshiakiTatsumi <octopusgarden0617@gmail.com>
2016-10-26 11:46:06 +0000

    task final

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -7,7 +7,7 @@ class MicropostsController < ApplicationController
       flash[:success] = "Micropost created!"
       redirect_to root_url
     else
-      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
+      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc).page(params[:page])
       render 'static_pages/home'
     end
   end
@@ -21,10 +21,7 @@ class MicropostsController < ApplicationController
   end
   
   private
-  def micropost_params
-    params.require(:micropost).permit(:content)
-  end
-  
+
   def micropost_params
     params.require(:micropost).permit(:content, :picture)
   end
